b4170a3a0d76b113282574b97f19de296c95f0a2
Stephen Oryema <148610430+OryemaStephen@users.noreply.github.com>
2024-08-25 08:26:45 +0300

    Formated code

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ import React from 'react'
 const Dashboard = React.lazy(() => import('./views/dashboard/Dashboard'))
 const Charts = React.lazy(() => import('./views/charts/Charts'))
 const Users = React.lazy(() => import('./views/users/Users'))
+const Questions = React.lazy(() => import('./views/Questions/Questions'))
 const Colors = React.lazy(() => import('./views/theme/colors/Colors'))
 
 const routes = [
@@ -10,7 +11,7 @@ const routes = [
   { path: '/dashboard', name: 'Dashboard', element: Dashboard },
   { path: '/charts', name: 'Charts', element: Charts },
   { path: '/users', name: 'Users', element: Users },
-  { path: '/questions', name: 'Questions Settings', element: Colors },
+  { path: '/questions', name: 'Questions Settings', element: Questions },
   { path: '/aiassistant', name: 'Ai Assistant', element: Users },
   { path: '/subscription', name: 'Subscription', element: Users },
   { path: '/organization', name: 'Organization', element: Colors },
