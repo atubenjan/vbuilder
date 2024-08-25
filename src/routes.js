c649abcf724940e99ed878ebe6fc6a2a79b1b1c5
Stephen Oryema <148610430+OryemaStephen@users.noreply.github.com>
2024-08-25 21:59:24 +0300

    Updated subscription and organization pages

--- src/routes.js
+++ src/routes.js
@@ -5,7 +5,8 @@ const Charts = React.lazy(() => import('./views/charts/Charts'))
 const Users = React.lazy(() => import('./views/users/Users'))
 const AiAssistant = React.lazy(() => import('./views/Ai/AiAssistant'))
 const Questions = React.lazy(() => import('./views/Questions/Questions'))
-const Colors = React.lazy(() => import('./views/theme/colors/Colors'))
+const Subscription = React.lazy(() => import('./views/Subscription/Subscription'))
+const Organization = React.lazy(() => import('./views/Organization/Organization'))
 
 const routes = [
   { path: '/', exact: true, name: 'Home' },
@@ -14,8 +15,8 @@ const routes = [
   { path: '/users', name: 'Users', element: Users },
   { path: '/questions', name: 'Questions Settings', element: Questions },
   { path: '/aiassistant', name: 'Ai Assistant', element: AiAssistant },
-  { path: '/subscription', name: 'Subscription', element: Users },
-  { path: '/organization', name: 'Organization', element: Colors },
+  { path: '/subscription', name: 'Subscription', element: Subscription },
+  { path: '/organization', name: 'Organization', element: Organization },
 ]
 
 export default routes
