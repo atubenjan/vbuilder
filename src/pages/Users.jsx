eb214ba575d3cec30a5bf96ce0f9da04560775d7
Stephen Oryema <148610430+OryemaStephen@users.noreply.github.com>
2024-08-30 12:55:51 +0300

    Updated home, user and organization pages to incorporate new components

--- src/pages/Users.jsx
+++ src/pages/Users.jsx
@@ -2,8 +2,9 @@ import React from 'react';
 import Footer from '../components/Footer';
 import PageTitle from '../components/PageTitle';
 import profile from '../assets/profile.jpg';
-import AddQuestion from '../components/AddQuestions';
-import QuestionsTable from '../components/QuestionsTable';
+// import AddQuestion from '../components/AddQuestions';
+// import QuestionsTable from '../components/QuestionsTable';
+import UserTable from '../components/UserTable';
 
 const Users = () => {
   return (
@@ -12,8 +13,9 @@ const Users = () => {
       <div className="flex-grow w-full h-full text-black bg-white">
         <PageTitle text="Users" showSearch={false} image={profile} />
         <div className="w-full p-4 pt-20">
-          <AddQuestion />
-          <QuestionsTable />
+          <UserTable />
+          {/* <AddQuestion />
+          <QuestionsTable /> */}
         </div>
       </div>
 
