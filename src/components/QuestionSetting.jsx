19934ded9eb22a0b0513ffd45b69f66e55677c61
Stephen Oryema <148610430+OryemaStephen@users.noreply.github.com>
2024-08-31 10:41:20 +0300

    Added subscription cards

--- src/components/QuestionSetting.jsx
+++ src/components/QuestionSetting.jsx
@@ -124,7 +124,7 @@ const QuestionSetting = () => {
               />
               <div className="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-slate-700 dark:peer-focus:ring-slate-700 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-slate-700 peer-checked:bg-slate-700"></div>
               <span className="text-sm font-medium text-gray-900 ms-3 dark:text-gray-300">
-                Essay Questions
+                Drag and Drop
               </span>
             </label>
           </div>
